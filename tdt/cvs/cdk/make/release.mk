53b8919d3b18ba4f03fe67688047a45c3a85a00b
ducktrick <goofmann@freenet.de>
2013-03-10 15:45:55 +0100

    djmount binary ins ziehlverzeichniss verschieben

--- tdt/cvs/cdk/make/release.mk
+++ tdt/cvs/cdk/make/release.mk
@@ -459,6 +459,7 @@ release_hl101: release_common_utils
 	cp -R $(buildprefix)/root/release/Scripte/emu $(prefix)/release/var/config/
 	cp -R $(buildprefix)/root/release/Scripte/system $(prefix)/release/var/config/
 	cp -R $(buildprefix)/root/release/Scripte/tools $(prefix)/release/var/config/
+	cp -f $(prefix)/cdkroot/usr/bin/djmount $(prefix)/release/usr/bin/
 	mkdir $(prefix)/release/var/keys
 	mkdir $(prefix)/release/var/keys/Benutzerdaten
 	mkdir $(prefix)/release/var/keys/Benutzerdaten/.emu
