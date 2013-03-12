720671c630814669bd3a7d6ca88c2c514cdbf496
ducktrick <goofmann@freenet.de>
2013-03-12 16:27:31 +0100

    Board p0211 Patch hinzugefügt und gpio Patch Fix

--- tdt/cvs/cdk/make/linux-kernel.mk
+++ tdt/cvs/cdk/make/linux-kernel.mk
@@ -122,7 +122,8 @@ HL101_PATCHES_24 = $(COMMONPATCHES_24) \
 		linux-usbwait123_stm24.patch \
 		linux-sh4-stmmac_stm24$(PATCH_STR).patch \
 		linux-sh4-i2c-st40-pio_stm24$(PATCH_STR).patch \
-		$(if $(P0207),linux-sh4-fortis_hdbox_i2c_st40_stm24$(PATCH_STR).patch)
+		$(if $(P0207),linux-sh4-fortis_hdbox_i2c_st40_stm24$(PATCH_STR).patch) \
+		$(if $(P0211),mb442.patch)
 
 VIP2_PATCHES_24  = $(COMMONPATCHES_24) \
 		linux-sh4-vip2_setup_stm24$(PATCH_STR).patch \
