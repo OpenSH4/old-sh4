70b8986d123d29219a118836c28b8bc7760d98c3
ducktrick <goofmann@freenet.de>
2013-03-23 11:34:57 +0100

    Enigma2 AAF Display String geändert

--- tdt/cvs/cdk/make/enigma2-nightly.mk
+++ tdt/cvs/cdk/make/enigma2-nightly.mk
@@ -54,6 +54,7 @@ $(DEPDIR)/enigma2-nightly.do_prepare:
 	$(if $(IPBOX9900),cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-ipbox.diff" )
 	$(if $(IPBOX99),cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-ipbox.diff" )
 	$(if $(IPBOX55),cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/enigma2-ipbox.diff" )
+	$(if $(HL101),cd $(appsdir)/enigma2-nightly && patch -p1 < "../../cdk/Patches/vfd.patch" )
 	cp -ra $(appsdir)/enigma2-nightly $(appsdir)/enigma2-nightly.patched
 	touch $@
 
