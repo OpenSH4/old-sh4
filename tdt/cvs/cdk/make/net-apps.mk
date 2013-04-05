79570ac885607939ad30287c32fbaf2bc487ce45
root <root@SUSE.site>
2013-04-05 22:01:40 +0200

    Fix Samba-Eintrag. Danke Ducktrick :-)

--- tdt/cvs/cdk/make/net-apps.mk
+++ tdt/cvs/cdk/make/net-apps.mk
@@ -130,8 +130,8 @@ $(DEPDIR)/samba.do_prepare: bootstrap @DEPENDS_samba@
 $(DEPDIR)/samba.do_compile: $(DEPDIR)/samba.do_prepare
 	export PATH=$(hostprefix)/bin:$(PATH) && \
 	cd @DIR_samba@ && \
-	cp -f /usr/include/lber* source3/	# Etwas unsauber, aber lber*.h und ldap*.h fehlen beim
-	cp -f /usr/include/ldap* source3/	# Bauen von Samba. Hoffe das klappt bei jedem :-)
+	cp -f /usr/include/lber* source3/ && \
+	cp -f /usr/include/ldap* source3/ && \
 		cd source3 && \
 		./autogen.sh && \
 		$(BUILDENV) \
