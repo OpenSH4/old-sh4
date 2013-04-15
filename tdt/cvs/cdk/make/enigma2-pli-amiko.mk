199212e1f76bc5da8d5a240d3f009b2137ce8ee5
root <root@SUSE.site>
2013-04-15 20:39:54 +0200

    Da hatte sich was eingeschlcihen, was da nciht reingehört.

--- tdt/cvs/cdk/make/enigma2-pli-amiko.mk
+++ tdt/cvs/cdk/make/enigma2-pli-amiko.mk
@@ -102,13 +102,3 @@ enigma2-pli-amiko-clean enigma2-pli-amiko-distclean:
 	rm -rf $(appsdir)/enigma2-nightly.newest
 	rm -rf $(appsdir)/enigma2-nightly.org
 	rm -rf $(appsdir)/enigma2-nightly.patched
-
-#
-# dvb/libdvbsi++
-#
-$(appsdir)/dvb/libdvbsi++/config.status: bootstrap
-	cd $(appsdir)/dvb/libdvbsi++ && $(CONFIGURE) CPPFLAGS="$(CPPFLAGS) -I$(driverdir)/dvb/include"
-
-$(DEPDIR)/libdvbsi++: $(appsdir)/dvb/libdvbsi++/config.status
-	$(MAKE) -C $(appsdir)/dvb/libdvbsi++ all install
-	touch $@
