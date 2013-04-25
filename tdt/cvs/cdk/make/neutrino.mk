eef0c77b07c5660b2b5a37145d602ed4502e3418
mcbane <dirk_1987@web.de>
2013-04-25 19:16:53 +0200

    Graph-LCD Menü in TeamCS-Patch integriert. neutrino.mk bis zu der Revision aktualisiert, wo die Patches noch komplett ohne Hinweise/Fehler funktionieren.

--- tdt/cvs/cdk/make/neutrino.mk
+++ tdt/cvs/cdk/make/neutrino.mk
@@ -346,7 +346,7 @@ $(DEPDIR)/neutrino-hd2-exp.do_prepare: | bootstrap $(EXTERNALLCD_DEP) libfreetyp
 	echo " 0) Newest		- NHD2 libplayer3    			(Can fail due to outdated patch)"; \
 	echo " 1) Newest (TeamCS)	- NHD2 libplayer3 + TeamCS-Menu   	(Can fail due to outdated patch)"; \
 	echo " 2) inactive"; \
-	echo " 3) Sun,  21 April 2013	- NHD2 libplayer3 + TeamCS-Menu		(SVN 1318)"; \
+	echo " 3) Wed,  24 April 2013	- NHD2 libplayer3 + TeamCS-Menu		(SVN 1328)"; \
 	echo "========================================================================================================"; \
 	echo "Media Framwork : $(MEDIAFW) (MediaFW will always be libeplayer3 for NHD2)"; \
 	echo "External LCD   : $(EXTERNALLCD)"; \
@@ -354,7 +354,7 @@ $(DEPDIR)/neutrino-hd2-exp.do_prepare: | bootstrap $(EXTERNALLCD_DEP) libfreetyp
 	[ "$$REPLY" == "0" ] && NHDselect=0; \
 	[ "$$REPLY" == "1" ] && NHDselect=1; \
 	[ "$$REPLY" == "2" ] && NHDselect=2; \
-	[ "$$REPLY" == "3" ] && NHDselect=3 && REVISION=1318; \
+	[ "$$REPLY" == "3" ] && NHDselect=3 && REVISION=1328; \
 	echo "Revision       : "$$REVISION; \
 	echo "";\
 	rm -rf $(appsdir)/neutrino-hd2-exp; \
