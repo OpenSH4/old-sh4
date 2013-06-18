770e2077f7a1b1cc973220515017ad0e1b39673f
mcbane <dirk_1987@web.de>
2013-06-18 09:32:51 +0200

    Fehlende Dateien nachgereicht.

---
+++ tdt/cvs/cdk/Patches/TeamCS/src/gui/teamcsoverclock.h
@@ -0,0 +1,36 @@
+#ifndef __teamcsoverclock__
+#define __teamcsoverclock__
+
+#include <string>
+#include <vector>
+
+#include <driver/framebuffer.h>
+#include <system/lastchannel.h>
+#include <system/setting_helpers.h>
+
+using namespace std;
+
+// unser eigenes Menu wird von CMenuTarget abgeleitet
+class teamcsoverclock : public CMenuTarget
+{
+   private:
+
+      CFrameBuffer *frameBuffer;
+      int x;
+      int y;
+      int width;
+      int height;
+      int hheight,mheight;    // head/menu font height
+
+      void paint();
+
+   public:
+
+      // Konstruktor
+      teamcsoverclock();
+
+      void hide();
+      int exec(CMenuTarget* parent, const std::string & actionKey);
+      
+};
+#endif
