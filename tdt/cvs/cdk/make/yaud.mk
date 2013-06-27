85f809bedf3ab564bc50fab923296526a8558d40
ducktrick <trick@trick-Lenovo-IdeaPad-S10-2.(none)>
2013-06-27 20:22:37 +0200

    Add Ducktrick.mk Release Informationen für MultiImage

--- tdt/cvs/cdk/make/yaud.mk
+++ tdt/cvs/cdk/make/yaud.mk
@@ -122,8 +122,8 @@ yaud-enigma2-pli-amiko: yaud-none host_python lirc \
 	@TUXBOX_YAUD_CUSTOMIZE@
 
 yaud-ducktrick-multiimage: yaud-none host_python lirc \
-		boot-elf remote firstboot enigma2-pli-amiko enigma2-plugins release e2-pli-plugins \
-		neutrino-hd2-exp release_neutrino_nightly
+		boot-elf remote firstboot enigma2-pli-amiko enigma2-plugins e2-pli-plugins \
+		neutrino-hd2-exp ducktrick
 	@TUXBOX_YAUD_CUSTOMIZE@
 
 yaud-xbmc-nightly: yaud-none host_python boot-elf firstboot xbmc-nightly release_xbmc
