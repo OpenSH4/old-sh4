37666255d72b8298ed82734cc6302a07d0899e7d
ducktrick <trick@trick-Lenovo-IdeaPad-S10-2.(none)>
2013-08-05 19:22:58 +0200

    Fixed Neutrino E2 ChannelBackup Script

--- tdt/cvs/cdk/root/release/Scripte/tools/sender_install.sh
+++ tdt/cvs/cdk/root/release/Scripte/tools/sender_install.sh
@@ -1,6 +1,6 @@
 echo "Backups NHD2/E2 Installieren"
 echo "warten bitte . . . Installiere Daten"
+# Senderlisten und Settings Files werden aus /tmp installiert
 tar -xzf /tmp/*.tar.gz -C /
-tar -xzf /Enigma2_System_Ordner/Backups/*.tar.gz
 echo "Installation abgeschlossen, System Neustarten um Aktuelle Senderlisten/Configs zu verwenden"
 exit 0
--- tdt/cvs/cdk/root/release/Scripte/tools/sender_sichern.sh
+++ tdt/cvs/cdk/root/release/Scripte/tools/sender_sichern.sh
@@ -1,7 +1,14 @@
 echo "Backup der Enigma-Senderlisten erstellen"
 echo "warten bitte . . . Comprimiere Daten"
-/bin/tar -czf /Enigma2_System_Ordner/Backups/Senderliste_Backup.E2.tar.gz /etc/enigma2 --exclude=/etc/enigma2/settings
-echo "Backup der Neutrino-Einstellungen + Senderliste erstellen"
-/bin/tar -czf /Enigma2_System_Ordner/Backups/Config_Backup.NHD2.tar.gz /var/tuxbox/config/
+echo " | Vorbereitung ..."
+SYSTEM=`cat /var/config/subsystem`
+# Download Settings File
+if [ $SYSTEM = Enigma2 ]; then
+	echo "Backup der Enigma2 Senderliste erstellen"
+	/bin/tar -czf /Enigma2_System_Ordner/Backups/Senderliste_Backup.E2.tar.gz /etc/enigma2 --exclude=/etc/enigma2/settings
+else
+	echo "Backup der Neutrino-Einstellungen + Senderliste erstellen"
+	/bin/tar -czf /Enigma2_System_Ordner/Backups/Config_Backup.NHD2.tar.gz /var/tuxbox/config/
+fi
 echo "Backup abgeschlossen, Datei per FTP aus dem /Enigma2_System_Ordner/Backups Ordner kopieren"
 exit 0
