c493b3c578dde778d003cd5643593c69db1f50aa
Trick <Goofmann@freenet.de>
2013-11-17 19:25:59 +0100

    release ducktrick fix

--- tdt/cvs/cdk/make/release_ducktrick.mk
+++ tdt/cvs/cdk/make/release_ducktrick.mk
@@ -603,7 +603,7 @@ release_ducktrick_install:
 # The main target depends on the model.
 # IMPORTANT: it is assumed that only one variable is set. Otherwise the target name won't be resolved.
 #
-$(DEPDIR)/release_ducktrick_ducktrick: \
+$(DEPDIR)/release_ducktrick: \
 $(DEPDIR)/%release_ducktrick: release_ducktrick_base release_ducktrick_$(TF7700)$(HL101)$(VIP1_V2)$(VIP2_V1)$(UFS910)$(UFS912)$(UFS913)$(SPARK)$(SPARK7162)$(UFS922)$(OCTAGON1008)$(FORTIS_HDBOX)$(ATEVIO7500)$(HS7810A)$(HS7110)$(WHITEBOX)$(CLASSIC)$(CUBEREVO_MINI)$(CUBEREVO_MINI2)$(CUBEREVO_MINI_FTA)$(CUBEREVO_250HD)$(CUBEREVO_2000HD)$(CUBEREVO_9500HD)$(HOMECAST5101)$(IPBOX9900)$(IPBOX99)$(IPBOX55)$(ADB_BOX) release_install
 	touch $@
 
