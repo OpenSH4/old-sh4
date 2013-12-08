7b9bfca20d3c4f07937add3e00f8570177b273a1
root <root@linux-ytzg.site>
2013-12-08 11:44:03 +0100

    NHD Team-CS-Menü: - Neue FBs und Empfänger hinzugefügt

--- tdt/cvs/cdk/Patches/TeamCS/src/gui/teamcsfb.cpp
+++ tdt/cvs/cdk/Patches/TeamCS/src/gui/teamcsfb.cpp
@@ -41,79 +41,72 @@
 
 teamcsfb::teamcsfb()
 {
-   frameBuffer = CFrameBuffer::getInstance();
-   width = 600;
-   hheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
-   mheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU]->getHeight();
-   height = hheight+13*mheight+ 10;
+frameBuffer = CFrameBuffer::getInstance();
+width = 600;
+hheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU_TITLE]->getHeight();
+mheight = g_Font[SNeutrinoSettings::FONT_TYPE_MENU]->getHeight();
+height = hheight+13*mheight+ 10;
 
-   x=(((g_settings.screen_EndX- g_settings.screen_StartX)-width) / 2) + g_settings.screen_StartX;
-   y=(((g_settings.screen_EndY- g_settings.screen_StartY)-height) / 2) + g_settings.screen_StartY;
+x=(((g_settings.screen_EndX- g_settings.screen_StartX)-width) / 2) + g_settings.screen_StartX;
+y=(((g_settings.screen_EndY- g_settings.screen_StartY)-height) / 2) + g_settings.screen_StartY;
 }
 
 
 int teamcsfb::exec(CMenuTarget* parent, const std::string &actionKey)
 {
-   int res = menu_return::RETURN_REPAINT;
-   int shortcutTeamCS = 1;
-   FILE *cprompt;
-   char ctext [4096];
-   char cprompthint[4096]={0};
-   neutrino_msg_t      msg;
-   neutrino_msg_data_t data;
-   unsigned long long TimeoutEnd;
+int res = menu_return::RETURN_REPAINT;
+int shortcutTeamCS = 1;
+FILE *cprompt;
+char ctext [4096];
+char cprompthint[4096]={0};
+neutrino_msg_t      msg;
+neutrino_msg_data_t data;
+unsigned long long TimeoutEnd;
 
 
-   if (parent)
-   {
-      parent->hide();
-   }
-   paint();
+if (parent)
+{
+	parent->hide();
+}
+paint();
 
-  if (actionKey.empty())
-  {
-      //Menu aufbauen
-      CMenuWidget* teamcsfb = new CMenuWidget(LOCALE_TEAMCS_HEAD, NEUTRINO_ICON_SETTINGS);
+if (actionKey.empty())
+{
+	//Menu aufbauen
+	CMenuWidget* teamcsfb = new CMenuWidget(LOCALE_TEAMCS_HEAD, NEUTRINO_ICON_SETTINGS);
 	teamcsfb->addItem(GenericMenuBack);
-        teamcsfb->addItem( new CMenuSeparator(CMenuSeparator::LINE) );
+	teamcsfb->addItem( new CMenuSeparator(CMenuSeparator::LINE) );
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_REMOTEHEAD, true, "", this, "remotehead", NULL, NULL , "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPNEU, true, "", this, "vipneu", CRCInput::RC_red, NEUTRINO_ICON_BUTTON_RED, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPNEUGRUEN, true, "", this, "vipneugruen", CRCInput::RC_green, NEUTRINO_ICON_BUTTON_GREEN, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPALT, true, "", this, "vipalt", CRCInput::RC_yellow, NEUTRINO_ICON_BUTTON_YELLOW, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPALTGRUEN, true, "", this, "vipaltgruen", CRCInput::RC_blue, NEUTRINO_ICON_BUTTON_BLUE, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_OPTI, true, "", this, "opti", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_PINGO, true, "", this, "pingo", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_TECHNOFB, true, "", this, "technofb", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_MCEFB, true, "", this, "mcefb", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuSeparator(CMenuSeparator::LINE) );
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_IRHEAD, true, "", this, "irhead", NULL, NULL , "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_STM, true, "", this, "stm", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_MCE2005, true, "", this, "mce2005", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_TECHNO, true, "", this, "techno", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->addItem( new CMenuSeparator(CMenuSeparator::LINE) );
+	teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_NEUSTART, true, "", this, "neustart", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	
 
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPNEU, true, "", this, "vipneu", CRCInput::RC_red, NEUTRINO_ICON_BUTTON_RED, "nix", LOCALE_HELPTEXT_NIX));
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPNEUGRUEN, true, "", this, "vipneugruen", CRCInput::RC_green, NEUTRINO_ICON_BUTTON_GREEN, "nix", LOCALE_HELPTEXT_NIX));
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPALT, true, "", this, "vipalt", CRCInput::RC_yellow, NEUTRINO_ICON_BUTTON_YELLOW, "nix", LOCALE_HELPTEXT_NIX));
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_VIPALTGRUEN, true, "", this, "vipaltgruen", CRCInput::RC_blue, NEUTRINO_ICON_BUTTON_BLUE, "nix", LOCALE_HELPTEXT_NIX));
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_OPTI, true, "", this, "opti", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_PINGO, true, "", this, "pingo", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
-      teamcsfb->addItem( new CMenuForwarderItemMenuIcon(LOCALE_TEAMCSFB_NEUSTART, true, "", this, "neustart", CRCInput::convertDigitToKey(shortcutTeamCS++), NULL, "nix", LOCALE_HELPTEXT_NIX));
+	teamcsfb->exec (NULL, "");
+	teamcsfb->hide ();
+	delete teamcsfb;
 
-      teamcsfb->exec (NULL, "");
-      teamcsfb->hide ();
-      delete teamcsfb;
+	
+}
+else if (actionKey == "vipneu")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
 
-      
-   }
-   else if (actionKey == "vipneu")
-   {
-      //CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
-
-      	system("cp /etc/lircd_neu.conf /etc/lircd.conf; echo neu > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB2.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_neu.conf /var/tuxbox/config/keymap.conf; echo vip2 > /var/config/boxtype");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP neu Mode Rot");	
-      	hintBox->paint();
-
-		while( msg != CRCInput::RC_ok )
-		{
-		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
-		usleep(5000);
-		}
-
-      	hintBox->hide();
-      	delete hintBox;
-   }	
-   else if (actionKey == "vipneugruen")
-   {
-
-      	system("cp /etc/lircd_neu_gruen.conf /etc/lircd.conf; echo neu > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB2.xml /usr/local/share/enigma2/keymap.xml;  rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_neu.conf /var/tuxbox/config/keymap.conf; echo vip2 > /var/config/boxtype");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP neu Mode Gruen");	
-      	hintBox->paint();
+		system("cp /etc/lircd_neu.conf /etc/lircd.conf; echo neu > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB2.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_neu.conf /var/tuxbox/config/keymap.conf; echo vip2 > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP neu Mode Rot");	
+		hintBox->paint();
 
 		while( msg != CRCInput::RC_ok )
 		{
@@ -121,31 +114,15 @@ int teamcsfb::exec(CMenuTarget* parent, const std::string &actionKey)
 		usleep(5000);
 		}
 
-      	hintBox->hide();
-      	delete hintBox;
-   }
-   else if (actionKey == "vipalt")
-   {
+		hintBox->hide();
+		delete hintBox;
+}	
+else if (actionKey == "vipneugruen")
+{
 
-      	system("cp /etc/lircd_alt.conf /etc/lircd.conf; echo alt > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB1.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_volume.conf /var/tuxbox/config/keymap.conf; echo vip1 > /var/config/boxtype");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP alt Mode Rot");	
-      	hintBox->paint();
-
-		while( msg != CRCInput::RC_ok )
-		{
-		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
-		usleep(5000);
-		}
-
-      	hintBox->hide();
-      	delete hintBox;   
-   }
-   else if (actionKey == "vipaltgruen")
-   {
-
-      	system("cp /etc/lircd_alt_gruen.conf /etc/lircd.conf; echo alt > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB1.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_volume.conf /var/tuxbox/config/keymap.conf; echo vip1 > /var/config/boxtype");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP alt Mode Gruen");	
-      	hintBox->paint();
+		system("cp /etc/lircd_neu_gruen.conf /etc/lircd.conf; echo neu > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB2.xml /usr/local/share/enigma2/keymap.xml;  rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_neu.conf /var/tuxbox/config/keymap.conf; echo vip2 > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP neu Mode Gruen");	
+		hintBox->paint();
 
 		while( msg != CRCInput::RC_ok )
 		{
@@ -153,33 +130,15 @@ int teamcsfb::exec(CMenuTarget* parent, const std::string &actionKey)
 		usleep(5000);
 		}
 
-      	hintBox->hide();
-      	delete hintBox;
-   }
-   else if (actionKey == "opti")
-   {
-      //CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "vipalt")
+{
 
-      	system("cp /etc/lircd_opti.conf /etc/lircd.conf; echo opti > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_Opti.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_opti.conf /var/tuxbox/config/keymap.conf; echo opti > /var/config/boxtype");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB Opticum");	
-      	hintBox->paint();
-
-		while( msg != CRCInput::RC_ok )
-		{
-		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
-		usleep(5000);
-		}
-
-      	hintBox->hide();
-      	delete hintBox;
-   }	
-   else if (actionKey == "pingo")
-   {
-      //CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
-
-      	system("cp /etc/lircd_pingolux.conf /etc/lircd.conf; echo neu > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_Opti.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; echo Pingolux > /var/config/boxtype");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB Pingolux gesetzt");	
-      	hintBox->paint();
+		system("cp /etc/lircd_alt.conf /etc/lircd.conf; echo alt > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB1.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_volume.conf /var/tuxbox/config/keymap.conf; echo vip1 > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP alt Mode Rot");	
+		hintBox->paint();
 
 		while( msg != CRCInput::RC_ok )
 		{
@@ -187,16 +146,100 @@ int teamcsfb::exec(CMenuTarget* parent, const std::string &actionKey)
 		usleep(5000);
 		}
 
-      	hintBox->hide();
-      	delete hintBox;
-   }	
-   else if (actionKey == "neustart")
-   {
-      //CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+		hintBox->hide();
+		delete hintBox;   
+}
+else if (actionKey == "vipaltgruen")
+{
 
-      	system("/var/config/shutdown/reboot.sh &");
-      	CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "System wird jetzt neugestartet");	
-      	hintBox->paint();
+		system("cp /etc/lircd_alt_gruen.conf /etc/lircd.conf; echo alt > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB1.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_volume.conf /var/tuxbox/config/keymap.conf; echo vip1 > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB ArgusVIP alt Mode Gruen");	
+		hintBox->paint();
+
+		while( msg != CRCInput::RC_ok )
+		{
+		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		usleep(5000);
+		}
+
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "opti")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("cp /etc/lircd_opti.conf /etc/lircd.conf; echo opti > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_Opti.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; cp /var/tuxbox/config/keymap_opti.conf /var/tuxbox/config/keymap.conf; echo opti > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB Opticum");	
+		hintBox->paint();
+
+		while( msg != CRCInput::RC_ok )
+		{
+		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		usleep(5000);
+		}
+
+		hintBox->hide();
+		delete hintBox;
+}	
+else if (actionKey == "pingo")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("cp /etc/lircd_pingolux.conf /etc/lircd.conf; echo neu > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_Opti.xml /usr/local/share/enigma2/keymap.xml; rm -f /var/tuxbox/config/keymap.conf; echo Pingolux > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB Pingolux gesetzt");	
+		hintBox->paint();
+
+		while( msg != CRCInput::RC_ok )
+		{
+		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		usleep(5000);
+		}
+
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "mcefb")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("cp /etc/lircd_mce2005.conf /etc/lircd.conf; echo mce2005 > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB2.xml /usr/local/share/enigma2/keymap.xml; echo mce2005 > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB MediaCenter gesetzt");	
+		hintBox->paint();
+
+		while( msg != CRCInput::RC_ok )
+		{
+		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		usleep(5000);
+		}
+
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "technofb")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("cp /etc/lircd_techno.conf /etc/lircd.conf; echo techno > /var/keys/Benutzerdaten/.system/fernbedienung; cp /usr/local/share/enigma2/keymap_FB2.xml /usr/local/share/enigma2/keymap.xml; echo techno > /var/config/boxtype");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "FB TechnoTrend gesetzt");	
+		hintBox->paint();
+
+		while( msg != CRCInput::RC_ok )
+		{
+		g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		usleep(5000);
+		}
+
+		hintBox->hide();
+		delete hintBox;
+}	
+else if (actionKey == "techno")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("echo techno > /var/config/system/remote");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "TechnoTrend USB Treiber Aktiviert - Reboot");	
+		hintBox->paint();
 
 		//while( msg != CRCInput::RC_ok )
 		//{
@@ -205,24 +248,76 @@ int teamcsfb::exec(CMenuTarget* parent, const std::string &actionKey)
 		//}
 		sleep(3);
 
-      	hintBox->hide();
-      	delete hintBox;
-   }	
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "mce2005")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
 
+		system("echo mce2005 > /var/config/system/remote");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "MCE USB Treiber Aktiviert - Reboot");	
+		hintBox->paint();
 
-   return res;
+		//while( msg != CRCInput::RC_ok )
+		//{
+		//g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		//usleep(5000);
+		//}
+		sleep(3);
+
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "stm")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("echo stm > /var/config/system/remote");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "STM Default Treiber Aktiviert - Reboot");	
+		hintBox->paint();
+
+		//while( msg != CRCInput::RC_ok )
+		//{
+		//g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		//usleep(5000);
+		//}
+		sleep(3);
+
+		hintBox->hide();
+		delete hintBox;
+}
+else if (actionKey == "neustart")
+{
+	//CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, g_Locale->getText(LOCALE_SERVICEMENU_GETPLUGINS_HINT));
+
+		system("/var/config/shutdown/reboot.sh &");
+		CHintBox * hintBox = new CHintBox(LOCALE_MESSAGEBOX_INFO, "System wird jetzt neugestartet");	
+		hintBox->paint();
+
+		//while( msg != CRCInput::RC_ok )
+		//{
+		//g_RCInput->getMsgAbsoluteTimeout( &msg, &data, &TimeoutEnd );
+		//usleep(5000);
+		//}
+		sleep(3);
+
+		hintBox->hide();
+		delete hintBox;
+}
+return res;
 }
 
 
 void teamcsfb::hide()
 {
-   frameBuffer->paintBackgroundBoxRel(x,y, width,height);
+frameBuffer->paintBackgroundBoxRel(x,y, width,height);
 }
 
 
 void teamcsfb::paint()
 {
-   printf("$Id: Menu Exp $\n");
+printf("$Id: Menu Exp $\n");
 }
 
 
