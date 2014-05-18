4b028210da4404849ad1f059ab0ba050fe43554a
Trick <goofmann@freenet.de>
2014-05-18 19:39:42 +0200

    Time.sh Update, Sommer winter zeit ohne neustart möglich, fixed icons im Scandby betrieb

--- tdt/cvs/cdk/root/release/Scripte/config/time.sh
+++ tdt/cvs/cdk/root/release/Scripte/config/time.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # TimeSet ArgusVIP-Mod by Ducktrick
 UHR=`cat /var/keys/Benutzerdaten/.system/uhr`
-TIMEZONE=`cat /var/keys/Benutzerdaten/.system/timezone`
 standby=0
+stats=0
 echo "run" > /var/config/time
-while sleep 10 
+while sleep 20 
 do
+TIMEZONE=`cat /var/keys/Benutzerdaten/.system/timezone`
 BOXOFF=`cat /var/config/boxoff`
+MODE=`cat /var/config/mode`
 STBY=`cat /proc/stb/avs/0/input`
 diff=$TIMEZONE
 hour=`date +"%H"`
@@ -19,23 +21,86 @@ hour=`expr $hour + $diff`
 if [ $hour -gt 12 ]; then
 hour=`expr $hour - 24`
 fp_control -s $hour:$minute:$second $day-$month-$year > /dev/null
+fp_control -i 34 1 > /dev/null
 fi
 if [ $STBY = scart ]; then # Hier beginnt die Anzeige wenn Box im standby
 	CLEAR=`cat /var/config/time`
 	if [ $standby = 0 ]; then
 		fp_control -c > /dev/null
+		sleep 2
+		fp_control -c > /dev/null
 		echo "run" > /var/config/time 
 		echo "Change Standby State too ON"
+		# Deaktiviert das dauer OC im Standby sonnst läuft die Uhr zu schnell ;)
+		if [ -e /var/keys/Benutzerdaten/.system/overclock ]; then
+       			OVERCLOCK=`cat /var/keys/Benutzerdaten/.system/overclock`
+       			if [ "$OVERCLOCK" = "300daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "333daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "366daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "400daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+			fi
+		fi
+	fi 
+	# Clear Display and Icons
+	if [ $stats = 0 ];then
+		fp_control -c > /dev/null
+		stats=1
 	fi
+	fp_control -i 34 1 > /dev/null
+	fp_control -i 36 1 > /dev/null
 	fp_control -s $hour:$minute:$second $day-$month-$year > /dev/null
 	fp_control -t "$day-$month" > /dev/null # Das ist die Display anzeige Monat und Tag
 	standby=1
+	# Deaktiviert den HDMI im Standby
+	/bin/stfbcontrol hd
+	if [ $MODE = BLANK ]; then
+		# set Display OFF
+		fp_control -l 0 > /dev/null
+	fi
 else # Hier beginnt die anzeige im Betrieb, nicht ändern
 	fp_control -s $hour:$minute:$second $day-$month-$year > /dev/null
 	fp_control -tm 24 > /dev/null
+	fp_control -i 36 0 > /dev/null
+	# Setzt neuen stats wert für nächsten Standby
+	if [ $stats = 1 ];then
+		stats=0
+	fi
+	if [ $MODE = BLANK ]; then
+		# set Display ON 
+		fp_control -l 1 > /dev/null
+	fi
 	if [ $standby = 1 ]; then
 		echo "anders" > /var/config/time 
 		echo "Change Standby State too OFF"
+		# Aktiviert den HDMI im Standby
+		/bin/stfbcontrol he
+		####### OnScreen Update Message #######
+		/var/config/system/updatecheck.sh &
+		# Aktiviert das dauer OC nach dem Standby wieder
+		if [ -e /var/keys/Benutzerdaten/.system/overclock ]; then
+       			OVERCLOCK=`cat /var/keys/Benutzerdaten/.system/overclock`
+       			if [ "$OVERCLOCK" = "300daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 25609 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "333daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 9475 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "366daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 31241 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "400daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 22790 > /proc/cpu_frequ/pll0_ndiv_mdiv
+			fi
+		fi
 	fi
 	standby=0
 fi
@@ -43,34 +108,124 @@ if [ $BOXOFF = OFF ]; then # Hier beginnt die Anzeige wenn Box Heruntergefahren 
 	CLEAR=`cat /var/config/time`
 	if [ $CLEAR = anders ]; then
 		fp_control -c > /dev/null
+		sleep 2
+		fp_control -c > /dev/null
 		echo "run" > /var/config/time 
 		echo "set run"
+		# Deaktiviert das dauer OC im Standby sonnst läuft die Uhr zu schnell ;)
+		if [ -e /var/keys/Benutzerdaten/.system/overclock ]; then
+       			OVERCLOCK=`cat /var/keys/Benutzerdaten/.system/overclock`
+       			if [ "$OVERCLOCK" = "300daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "333daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "366daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "400daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+			fi
+		fi
 	else
+	if [ $stats = 0 ];then
+		fp_control -c > /dev/null
+		stats=1
+	fi
 	echo "give Time String"
+	fp_control -i 34 1
+	fp_control -i 36 1
 	fp_control -s $hour:$minute:$second $day-$month-$year > /dev/null
 	fp_control -t "Turn OFF" #> /dev/null # Hier wird wenn die runtergefahren ist Turn OFf im Display gezeigt
+	# Deaktiviert den HDMI im Standby
+	/bin/stfbcontrol hd
 	fi
 elif [ $BOXOFF = BLANK ]; then
 	fp_control -c #Schalltet alle schriftzüge aus Im Display und bleibt dunkel
+	sleep 1
+	fp_control -c > /dev/null
+	fp_control -i 36 1 > /dev/null
+	fp_control -i 34 1 > /dev/null
+	# Deaktiviert den HDMI im Standby
+	/bin/stfbcontrol hd
+	fp_control -l 0 > /dev/null
 elif [ $BOXOFF = DATE ]; then
 	CLEAR=`cat /var/config/time`
 	if [ $CLEAR = anders ]; then
 		fp_control -c > /dev/null
+		sleep 2
+		fp_control -c > /dev/null
 		echo "run" > /var/config/time 
+		# Deaktiviert das dauer OC im Standby sonnst läuft die Uhr zu schnell ;)
+		if [ -e /var/keys/Benutzerdaten/.system/overclock ]; then
+       			OVERCLOCK=`cat /var/keys/Benutzerdaten/.system/overclock`
+       			if [ "$OVERCLOCK" = "300daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "333daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "366daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "400daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+			fi
+		fi
 	else
+	# Clear Display and Icons
+	if [ $stats = 0 ];then
+		fp_control -c > /dev/null
+		stats=1
+	fi
 	fp_control -s $hour:$minute:$second $day-$month-$year > /dev/null
-	fp_control -t "$day-$month" > /dev/null # Hier wird wenn die Box runtergefahren ist Datum und uhr angezeigt im Display
+	fp_control -i 34 1 > /dev/null
+	fp_control -i 36 1 > /dev/null
+	fp_control -t "$day-$month" > /dev/null # Hier wird wenn die Box runtergefahren ist Datum Uhr angezeigt im Display
+	# Deaktiviert den HDMI im Standby
+	/bin/stfbcontrol hd
 	fi
 elif [ $BOXOFF = DATEPLUS ]; then
 	CLEAR=`cat /var/config/time`
 	if [ $CLEAR = anders ]; then
 		fp_control -c > /dev/null
+		sleep 2
+		fp_control -c > /dev/null
 		echo "run" > /var/config/time 
+		# Deaktiviert das dauer OC im Standby sonnst läuft die Uhr zu schnell ;)
+		if [ -e /var/keys/Benutzerdaten/.system/overclock ]; then
+       			OVERCLOCK=`cat /var/keys/Benutzerdaten/.system/overclock`
+       			if [ "$OVERCLOCK" = "300daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "333daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "366daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+       			elif [ "$OVERCLOCK" = "400daueron" ]; then
+    				echo "overclock Aktiviert"
+   				echo 15110 > /proc/cpu_frequ/pll0_ndiv_mdiv
+			fi
+		fi
 	else
+	# Clear Display and Icons
+	if [ $stats = 0 ];then
+		fp_control -c > /dev/null
+		stats=1
+	fi
+	fp_control -i 34 1 > /dev/null
+	fp_control -i 36 1 > /dev/null
 	fp_control -s $hour:$minute:$second $day-$month-$year > /dev/null
 	fp_control -t "$day-$month" > /dev/null # Hier wird wenn die box runtergefahren ist das Datum und Turn OFF im Display als Wechselschrift angezeigt
 	sleep 30
 	fp_control -t "Turn OFF" > /dev/null
+	# Deaktiviert den HDMI im Standby
+	/bin/stfbcontrol hd
 	fi
 fi
 done
