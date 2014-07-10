450360e725f6c4798f4d22ef25cd9f001bc3dd0f
Trick <goofmann@freenet.de>
2014-07-10 12:39:47 +0200

    fp_control, Modload, rcS, Shutdown.sh angepasst für detect driver

--- tdt/cvs/apps/misc/tools/fp_control/fp_control.c
+++ tdt/cvs/apps/misc/tools/fp_control/fp_control.c
@@ -574,7 +574,7 @@ int getModel()
 	int         vLen            = -1;
 	eBoxType    vBoxType        = Unknown;
 
-	vFd = open("/var/config/fp_control", O_RDONLY);
+	vFd = open("/proc/Boxtype", O_RDONLY);
 	vLen = read (vFd, vName, cSize);
 
 	close(vFd);
@@ -602,11 +602,11 @@ int getModel()
 			vBoxType = Ufs922;
 		else if (!strncasecmp(vName,"tf7700hdpvr", 11))
 			vBoxType = Tf7700;
-		else if (!strncasecmp(vName,"aotom", 5))
+		else if (!strncasecmp(vName,"Vip1v2", 5))
 			vBoxType = Vip2;
-		else if (!strncasecmp(vName,"proton", 7))
+		else if (!strncasecmp(vName,"Vip1", 7))
 			vBoxType = Hl101;
-		else if (!strncasecmp(vName,"vip2-v1", 7))
+		else if (!strncasecmp(vName,"Vip2", 7))
 			vBoxType = Vip2;
 		else if (!strncasecmp(vName,"hdbox", 5))
 			vBoxType = HdBox;
--- tdt/cvs/cdk/root/config/shutdown/shutdown.sh
+++ tdt/cvs/cdk/root/config/shutdown/shutdown.sh
@@ -1,24 +1,74 @@
 #!/bin/sh
    /bin/echo "Herunterfahren" > /dev/vfd
    sleep 3
+if [ -e /var/config/subsystem ]; then
+   SYSTEM=`cat /var/config/subsystem`
+   if [ "$SYSTEM" = "Neutrino" ]; then
+      echo "killed self"
+   else
+      if [ -e /var/config/subswitch ]; then
+         echo "subswitch Aktiv"
+      else
+         echo "killed self"
+      fi
+   fi
+else
+   killall -9 enigma2
+fi
+   sleep 3
+   ##Bootlogo in HD
+if [ -e /var/config/subsystem ]; then
+   SYSTEM=`cat /var/config/subsystem`
+   if [ "$SYSTEM" = "Neutrino" ]; then
+      echo "############ Neutrino Shutdown, no Logo ################"
+   else   
+      if [ -e /var/config/subswitch ]; then
+         echo "subswitch Aktiv no Logo"
+      else
+         /usr/local/bin/dvbtest -4 -f l /boot/shutdown.mp4 &
+      fi
+   fi
+else   
+   /usr/local/bin/dvbtest -4 -f l /boot/shutdown.mp4 &
+fi
+   sleep 12
    echo "init lircd"
    killall -9 evremote2
    killall -9 lircd   
    killall -9 smbd &
-   /etc/init.d/portmap stop & 
+   /etc/init.d/portmap stop &
+   /usr/bin/lircd -P /ram/lircd.pid -o /ram/lircd 
    ##FB Laden
-   /usr/bin/lircd  
-   stfbcontrol a 0   
-fb=`cat /var/keys/Benutzerdaten/.system/fernbedienung`
-   if [ "$fb" = "neu" ]; then
-        /bin/evremote2 vip2 &
-   elif [ "$fb" = "alt" ]; then
-      /bin/evremote2 vip1 &
-   elif [ "$fb" = "opti" ]; then
-      /bin/evremote2 opti &
-   elif [ "$fb" = "Pungolux" ]; then
-      /bin/evremote2 Pingolux &
-   fi
+    stfbcontrol a 0   
+    fb=`cat /var/keys/Benutzerdaten/.system/fernbedienung`
+    DEEPSTANDBY=`cat /proc/Boxtype`
+       if [ "$DEEPSTANDBY" = "Vip2" ]; then
+		SETS="-s"
+       elif [ "$DEEPSTANDBY" = "Vip1v2" ]; then
+		SETS="-s"
+       elif [ "$DEEPSTANDBY" = "Vip1" ]; then
+		SETS="-x"
+       fi
+       if [ "$fb" = "neu" ]; then
+          echo "Fernbedienung NEU geladen"
+            /bin/evremote2 $SETS vip2 &
+       elif [ "$fb" = "alt" ]; then
+          echo "Fernbedienung ALT geladen"
+          /bin/evremote2 $SETS vip1 &
+       elif [ "$fb" = "opti" ]; then
+          echo "Fernbedienung OPTI geladen"
+          /bin/evremote2 $SETS opti &
+       elif [ "$fb" = "Pingulux" ]; then
+          echo "Fernbedienung Pingulux geladen"
+          /bin/evremote2 $SETS Pingulux &
+       elif [ "$fb" = "mce2005" ]; then
+          echo "Fernbedienung MediaCenter geladen"
+          /bin/evremote2 $SETS MediaCenter &
+       elif [ "$fb" = "techno" ]; then
+          echo "Fernbedienung TechnoTrend geladen"
+          /bin/evremote2 $SETS TechnoTrend &
+       fi
+    fi
    ##Module Entladen
    rmmod /lib/modules/boxtype.ko
    rmmod /lib/modules/cifs.ko
@@ -29,8 +79,6 @@ fb=`cat /var/keys/Benutzerdaten/.system/fernbedienung`
    rmmod /lib/modules/sth264pp.ko
    rmmod /lib/modules/mpeg2hw.ko
    ##Modul ende
-   umount -l /dev/sda1
-   umount -l /dev/sdb1
    sync
    killall -9 dvbtest
    # Aktiviere Display Uhr anzeige für Box OFF
@@ -40,45 +88,48 @@ fb=`cat /var/keys/Benutzerdaten/.system/fernbedienung`
       rm /tmp/autoswitch.tmp
       sleep 1
    fi
-   if [ -e /var/config/deepstandby ]; then
-      DEEPSTANDBY=`cat /var/config/deepstandby`
-      if [ "$DEEPSTANDBY" = "vip2" ]; then
-         sync
-         sleep 2
-         swapoff /dev/sda2
-         sleep 3
+
+   DEEPSTANDBY=`cat /proc/Boxtype`
+   if [ "$DEEPSTANDBY" = "Vip2" ]; then
+        sync
+	echo "umount FS"
+		swapoff /dev/sda3
+		umount /dev/sdb1
+		umount /dev/sda2
+		umount /sys
+		umount /ram
+	echo "umountet sda1 sdb1 sys ram swap"
+	 sync
          fp_control -d
-      else
+   elif [ "$DEEPSTANDBY" = "Vip1v2" ]; then
+        sync
+	echo "umount FS"
+		swapoff /dev/sda3
+		umount /dev/sdb1
+		umount /dev/sda2
+		umount /sys
+		umount /ram
+	echo "umountet sda1 sdb1 sys ram swap"
+	 sync
+         fp_control -d
+   else
          until false
          do
          file="/tmp/autoswitch.tmp"
          if [ -e $file ]; then
                  echo "Neustart" > /dev/vfd
-            sleep 2
-            swapoff /dev/ramzswap0
-            sleep 2
-                 sync
-            sleep 2
+		echo "umount FS"
+		swapoff /dev/sda3
+		umount /dev/sdb1
+		umount /dev/sda2
+		umount /dev/sda1
+		umount /sys
+		umount /ram
+		echo "umountet sda1 sdb1 sys ram swap"
+            	sleep 2
                  init 6
          else    
             sleep 3
          fi
          done
-      fi
-   else
-      until false
-      do
-      file="/tmp/autoswitch.tmp"
-      if [ -e $file ]; then
-                echo "Neustart" > /dev/vfd
-         sleep 2
-         swapoff /dev/ramzswap0
-         sleep 2
-                sync
-         sleep 2
-                init 6
-      else    
-         sleep 3
-      fi
-      done
    fi
