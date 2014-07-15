d607d339729629a175a04b40355daca67abddfbe
Trick <goofmann@freenet.de>
2014-07-15 10:16:01 +0200

    Detect Treiber Info Text Add

--- tdt/cvs/driver/detect/detect.c
+++ tdt/cvs/driver/detect/detect.c
@@ -24,6 +24,8 @@
 
 #include "detect.h"
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 /* Tuner register Vip2 reset for Tuner Scan 
    This use only by Boxtype = Vip2 detect  */
 static unsigned char fctl = 0;
@@ -70,9 +72,9 @@ void hc595_out(unsigned char ctls, int state)
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int detected_boxid;
-static char *Boxtype;
-static char *Tunertype;
-static char *Tunertype2;
+static char *Boxtype;		// Statisch Boxtype
+static char *Tunertype;		// Statisch Tuner Type 1
+static char *Tunertype2;	// Statisch Tuner Type 2 (Vip2)
 
 int read_info( char *page, char **start, off_t off,int count, int *eof, void *data );
 ssize_t write_info( struct file *filp, const char __user *buff,unsigned long len, void *data );
@@ -86,7 +88,7 @@ static int write_index;
 static int read_index;
 
 // -------------------------------------------------------------
-
+/* I2C Write Funktion */
 static int i2c_autodetect (struct i2c_adapter *adapter, unsigned char i2c_addr, unsigned char dev_addr)
 {
   	unsigned char buf[2] = { 0, 0 };
@@ -112,35 +114,44 @@ int TunerScan(char *Boxtype)
 	/* Check I2CBus and Tuner Address */
 	if(Boxtype == "Vip1") 
 	{
+	  	/* Bus 0 Check */
 		if((adap = i2c_get_adapter(I2C_TUNERBUS0)) == NULL) {
 			printk("[DETECT] -> Autodetection failed. I2C Tunerbus error\n");
 			return 0;
 		}
+		/* Ist 0x6a auf Bus 0 vorhanden setzen wir RB-Tuner */
 		if(i2c_autodetect(adap, RBTuner, 0x0) != -1 ) {
 			Tunertype = "stb6100";
 		}
+		/* Ist 0x68 auf Bus 0 vorhanden setzen wir ST-Tuner */
 		if(i2c_autodetect(adap, STTuner, 0x0) != -1 ) {
 			Tunertype = "stb6110x";
 		}
+		/* Ist 0x0c auf Bus 0 vorhanden setzen wir LG-Tuner */
 		if(i2c_autodetect(adap, LGCabel, 0x0) != -1 ) {
 			Tunertype = "lg031";
 		}
+		/* Ist 0x0f auf Bus 0 vorhanden setzen wir Sharp DVBT Tuner */
 		if(i2c_autodetect(adap, Sharpdvbt, 0x0) != -1 ) {
 			Tunertype = "sharp6465";
 		}
 	}
 	if(Boxtype == "Vip1v2") 
 	{
+	  	/* Bus 0 Check */
 		if((adap = i2c_get_adapter(I2C_TUNERBUS0)) == NULL) {
 			printk("[DETECT] -> Autodetection failed. I2C Tunerbus error\n");
 			return 0;
 		}
+		/* Ist 0x0c auf Bus 0 vorhanden setzen wir LG-Tuner */
 		if(i2c_autodetect(adap, LGCabel, 0x0) != -1 ) {
 			Tunertype = "lg031";
 		}
+		/* Ist 0x0f auf Bus 0 vorhanden setzen wir Sharp DVBT Tuner */
 		if(i2c_autodetect(adap, Sharpdvbt, 0x0) != -1 ) {
 			Tunertype = "sharp6465";
 		}
+		/* Ist 0x68 auf Bus 0 vorhanden setzen wir SharpS2-Tuner(Ist zwar die gleiche Adresse wie ST-Tuner, diese sind aber nicht im Vip1v2 und Vip2 verbaut ;)) */
 		if(i2c_autodetect(adap, SharpS2, 0x0) != -1 ) {
 			Tunertype = "sharp7306";
 		}
@@ -152,12 +163,15 @@ int TunerScan(char *Boxtype)
 			printk("[DETECT] -> Autodetection failed. I2C Tunerbus error\n");
 			return 0;
 		}
+		/* Ist 0x0c auf Bus 0 vorhanden setzen wir LG-Tuner */
 		if(i2c_autodetect(adap, LGCabel, 0x0) != -1 ) {
 			Tunertype = "lg031";
 		}
+		/* Ist 0x0f auf Bus 0 vorhanden setzen wir Sharp DVBT Tuner */
 		if(i2c_autodetect(adap, Sharpdvbt, 0x0) != -1 ) {
 			Tunertype = "sharp6465";
 		}
+		/* Ist 0x68 auf Bus 0 vorhanden setzen wir SharpS2-Tuner(Ist zwar die gleiche Adresse wie ST-Tuner, diese sind aber nicht im Vip1v2 und Vip2 verbaut ;)) */
 		if(i2c_autodetect(adap, SharpS2, 0x0) != -1 ) {
 			Tunertype = "sharp7306";
 		}
@@ -166,18 +180,21 @@ int TunerScan(char *Boxtype)
 			printk("[DETECT] -> Autodetection failed. I2C Tunerbus error\n");
 			return 0;
 		}
+		/* Ist 0x0c auf Bus 1 vorhanden setzen wir LG-Tuner */
 		if(i2c_autodetect(adap, LGCabel, 0x0) != -1 ) {
 			Tunertype2 = "lg031";
 		}
+		/* Ist 0x0f auf Bus 1 vorhanden setzen wir Sharp DVBT Tuner */
 		if(i2c_autodetect(adap, Sharpdvbt, 0x0) != -1 ) {
 			Tunertype2 = "sharp6465";
 		}
+		/* Ist 0x68 auf Bus 1 vorhanden setzen wir SharpS2-Tuner(Ist zwar die gleiche Adresse wie ST-Tuner, diese sind aber nicht im Vip1v2 und Vip2 verbaut ;)) */
 		if(i2c_autodetect(adap, SharpS2, 0x0) != -1 ) {
 			Tunertype2 = "sharp7306";
 		}
 	}
 	
-	
+	/* prink des Boxtype und des Tuners die ermittelt wurden */
 	if(Boxtype == "Vip1")
 		printk("[DETECT] -> Boxtype = %s \n[DETECT] -> Tuner = %s \n", Boxtype, Tunertype);
 	if(Boxtype == "Vip1v2")
@@ -194,21 +211,27 @@ static int detect_boxid(void)
 	int i2c40 = 0;
 	int i2c28 = 0;
 
+	/* Bus 2 Check ob verfügbar */
 	if((adap = i2c_get_adapter(I2C_BUS2)) == NULL) {
 		printk("[DETECT] -> Autodetection failed. I2C bus error\n");
 		return 0;
 	}
+	/* Bus 2 write auf Adresse 0x40 (Cimax Adresse(Hat nur Vip1 und Vip1v2)), wenn verfügbar set Status 1, anderfalls 3 */
   	if(i2c_autodetect(adap, I2C_0X40, 0x0) != -1 ) {
 		i2c40 = 1;	
 	} else {
 		i2c40 = 3;
 	}
+	/* Bus 2 write auf Adresse 0x28(Display Adresse(Hat nur Vip1v2 und Vip2)), wenn verfügbar set Status 1, anderfalls 3 */
   	if(i2c_autodetect(adap, I2C_0X28, 0x0) != -1 ) {
 		i2c28 = 1;
 	} else {
 		i2c28 = 3;
 	}
-	
+
+	/* Ist 0x40 vorhanden aber nicht 0x28(wegen fehlendem Display) so ist es ein Vip1(Opticum))
+	 * Ist 0x40 und 0x28 vorhanden so ist es ein Vip1v2
+	 */
 	if(i2c40 == 1) {
 		if(i2c28 == 1) {
 			Boxtype = "Vip1v2";
@@ -216,12 +239,13 @@ static int detect_boxid(void)
 			Boxtype = "Vip1";
 		}
 	}
+	/* Ist 0x40 nicht vorhanden aber 0x28 (wegen Fehlenden Ci Schacht) so ist es Vip2 */
 	if(i2c40 == 3) {
 		if(i2c28 == 1) {
 			Boxtype = "Vip2";
 		}
 	}
-
+	/* Ist Garnix vorhanden ERROR mit printk ausgabe*/
 	if(i2c40 == 3) {
 		if(i2c28 == 3) {
 			printk("[DETECT] -> error \n[DETECT] -> i2c40 = %d \n[DETECT] -> i2c28 = %d\n", i2c40, i2c28);
@@ -232,6 +256,7 @@ static int detect_boxid(void)
 	/* we need reset Tuner by Shift register for Vip2 
 	   4 = Tuner A Reset Pin on hc595
 	   1 = Tuner B Reset Pin on hc595		*/
+	/* Ist Boxtype Vip2 erkannt müssen wir die Tuner anders Resetten um sie nachfolgen peer I2C zu finden */
 	if(Boxtype == "Vip2") {
 		hc595_out (4, 0);
   		hc595_out (1, 0);
@@ -242,7 +267,7 @@ static int detect_boxid(void)
 	return TunerScan(Boxtype);
 }
 // -------------------------------------------------------------
-
+/* /proc/Boxtype und /proc/Tunertype1 sowie 2 können beschrieben werden in E2 per echo "Type" > /proc/Boxtype */
 ssize_t write_info( struct file *filp, const char __user *buff, unsigned long len, void *data )
 {
     int capacity = (MAX_LEN-write_index)+1;
@@ -259,7 +284,7 @@ ssize_t write_info( struct file *filp, const char __user *buff, unsigned long le
     info[write_index-1] = 0;
     return len;
 }
-
+/* Read Funktion für /proc/Boxtype */ 
 int read_info( char *page, char **start, off_t off, int count, int *eof, void *data )
 {
     int len;
@@ -277,7 +302,7 @@ int read_info( char *page, char **start, off_t off, int count, int *eof, void *d
 
     return len;
 }
-
+/* Read Funktion für /proc/Tunertype1 */ 
 int read_info_tuner1( char *page, char **start, off_t off, int count, int *eof, void *data )
 {
     int len;
@@ -295,7 +320,7 @@ int read_info_tuner1( char *page, char **start, off_t off, int count, int *eof, 
 
     return len;
 }
-
+/* Read Funktion für /proc/Tunertype2 */ 
 int read_info_tuner2( char *page, char **start, off_t off, int count, int *eof, void *data )
 {
     int len;
@@ -328,10 +353,10 @@ int __init detect_init(void)
 	 * vor dem init der avs (Boxtype) und des fe_core ermitteln
 	 */
 
-	detect1 = stpio_request_pin (2, 2, "detect1", STPIO_OUT); // need Vip2
-	detect2 = stpio_request_pin (2, 3, "detect2", STPIO_OUT);
-	detect4 = stpio_request_pin (2, 4, "detect4", STPIO_OUT); // need Vip2
-	detect3 = stpio_request_pin (2, 5, "detect3", STPIO_OUT);
+	detect1 = stpio_request_pin (2, 2, "detect1", STPIO_OUT); // Vip2 hc595 Pio
+	detect2 = stpio_request_pin (2, 3, "detect2", STPIO_OUT); // Standart Reset Pin Vip1, Vip1v2 und Vip2 hc595
+	detect4 = stpio_request_pin (2, 4, "detect4", STPIO_OUT); // Vip2 hc595 Pio
+	detect3 = stpio_request_pin (2, 5, "detect3", STPIO_OUT); // Cimax Boxtype Pio
 	
 	if ((detect1 == NULL) || (detect2 == NULL) || (detect3 == NULL) || (detect4 == NULL))
 	{
@@ -359,8 +384,8 @@ int __init detect_init(void)
 	 * Das benötigen nur Vip1 und Vip1v2, Vip2 Reset
 	 * Erfolgt über das Shift register beim Boxtype detect.
 	 */
-	stpio_set_pin(detect2, 0);
-	stpio_set_pin(detect2, 1);
+	stpio_set_pin(detect2, 0); // default Reset für Vip1 und Vip1v2
+	stpio_set_pin(detect2, 1); // default Reset für Vip1 und Vip1v2
 	
 	/* startet Boxtype und tuner Check */
 	detected_boxid = detect_boxid();
@@ -370,7 +395,6 @@ int __init detect_init(void)
 	 * wie AVS Cimax und fe-core (Tuner) nicht geladen werden können.
 	 * Danach gehts wie gehabt beim Boot weiter !!!
 	 */
-
 	stpio_free_pin (detect1);
 	stpio_free_pin (detect2);
 	stpio_free_pin (detect3);
@@ -379,6 +403,7 @@ int __init detect_init(void)
 	/* make /proc */
     	info = (char *)vmalloc( MAX_LEN );
     	memset( info, 0, MAX_LEN );
+	/* Erstellt /proc/Boxtype */
     	proc_entry = create_proc_entry( "Boxtype", 0644, NULL );
 
     	if (proc_entry == NULL)
@@ -393,7 +418,7 @@ int __init detect_init(void)
     	    proc_entry->read_proc = read_info;
     	    proc_entry->write_proc = write_info;
    	}
-
+	/* Erstellt /proc/Tunertype1 */
     	proc_entry_tuner1 = create_proc_entry( "Tunertype", 0644, NULL );
 
     	if (proc_entry_tuner1 == NULL)
@@ -408,7 +433,7 @@ int __init detect_init(void)
     	    proc_entry_tuner1->read_proc = read_info_tuner1;
     	    proc_entry_tuner1->write_proc = write_info;
    	}
-
+	/* /proc/Tunertype2 wird nur erstellt wenn beim Boxtype Vip2 gefunden wurde */
 	if(Boxtype == "Vip2") {
 		proc_entry_tuner2 = create_proc_entry( "Tunertype2", 0644, NULL );
 
--- tdt/cvs/driver/detect/detect.h
+++ tdt/cvs/driver/detect/detect.h
@@ -2,28 +2,28 @@
 #define _DETECT_H
 
 // Proc
-#define MAX_LEN       4096
+#define MAX_LEN       4096 	// Proc Buffer
 
 /* wir benoetigen die Pins zum Aktivieren der Buse */
-struct stpio_pin*	detect1;   // dieser Pin ist bei allen Boxen gleich...
-struct stpio_pin*	detect2;   // dieser Pin ist bei allen Boxen gleich...
-struct stpio_pin*	detect3;   // dieser Pin ist bei allen Boxen gleich...
-struct stpio_pin*	detect4;   // serial data Vip2
+struct stpio_pin*	detect1;   // Vip2 Shift register Pin...
+struct stpio_pin*	detect2;   // Vip1 Vip1v2 Tuner reset und shift Vip2 Pin ...
+struct stpio_pin*	detect3;   // Cimax Boxtype Pin 
+struct stpio_pin*	detect4;   // Vip2 Shift register Pin...
 
 /* I2C Check Address of Bus 2 for Boxtype */
-#define I2C_BUS2		2
-#define I2C_0X40		0x40
-#define I2C_0X28		0x28
+#define I2C_BUS2		2 	// I2C Bus 2
+#define I2C_0X40		0x40	// Cimax I2C Addresse
+#define I2C_0X28		0x28	// Display I2C Adresse
 
 /* I2C Check Bus0 & 1 for Tuner */
-#define I2C_TUNERBUS0		0
-#define I2C_TUNERBUS1		1
+#define I2C_TUNERBUS0		0	// I2C Bus 0
+#define I2C_TUNERBUS1		1	// I2C Bus 1
 
 /* Tuner Address from I2C */
-#define SharpS2			0x68
-#define RBTuner			0x6a
-#define STTuner			0x68
-#define LGCabel			0x0c
-#define Sharpdvbt		0x0f
+#define SharpS2			0x68	// SharpS2 Tuner I2C Adresse auf Bus 1 und/oder Bus2 (Vip2 und/oder Vip1v2)
+#define RBTuner			0x6a	// RB-Tuner I2C Adresse auf Bus 1 
+#define STTuner			0x68	// ST-Tuner I2C Adresse auf Bus 1 
+#define LGCabel			0x0c	// LG-Tuner I2C Adresse auf Bus 1 und/oder Bus2 (Vip2 und/oder Vip1v2)
+#define Sharpdvbt		0x0f	// Sharp DVB-T Tuner I2C Adresse auf Bus 1 und/oder Bus2 (Vip2 und/oder Vip1v2)
 
 #endif
